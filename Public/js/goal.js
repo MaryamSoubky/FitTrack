99d68920536e1fa857668f6d7d422754ed8d0d43
MaryamSoubky <156623912+MaryamSoubky@users.noreply.github.com>
2024-12-31 20:34:29 +0200

    Delete Public directory

--- Public/js/goal.js
+++
@@ -1,5 +0,0 @@
-// Placeholder for real-time goal tracking
-document.getElementById("goalForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Goal set successfully!");
-});
--- Public/js/script_admin.js
+++
@@ -1,4 +0,0 @@
-// Script to handle sidebar toggling
-document.querySelector('.toggle').addEventListener('click', () => {
-    document.querySelector('.sidebar').classList.toggle('close');
-});
